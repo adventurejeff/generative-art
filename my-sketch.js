ecd7020564d707465b994b28fa01b27fb899466a
Geoff Hill <geoffwhill@gmail.com>
2019-01-04 14:27:57 +1100

    Added grid of elements

--- my-sketch.js
+++ my-sketch.js
@@ -1,35 +1,77 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 256, 256 ]
+  dimensions: [ 1024, 1024 ], //or 'A4'/[x, y] 
+  //orentation: 'landscape'
+  //pixelsPerInch: 300, units: 'cm' 
+  //Noitce the , between each paramter
 };
 
+
 const sketch = () => {
-  return ({ context, width, height }) => {
-    //rectangle on the outside
-    context.fillStyle = 'orange';
-    context.fillRect(0, 0, width, height);
+  
 
-    //rectangle in the center
-    context.strokeStyle = 'white';
-    context.lineWidth = 14;
-    
-    //
-    context.strokeRect(width / 4, height / 4, width / 2, height / 2);
+  //Function declaration
+  const createGrid = () => {
+    const points = []; //create an array of points
+    const iterations = 5;
 
-    //
-    context.beginPath();
-    context.arc(width/2, height/2, 75, 0, 2 * Math.PI);
-    context.strokeStyle = 'red';
-    context.stroke();
-    
-    //
-    context.beginPath();
-    context.fillStyle = 'green';
-    context.fill();
-    context.fillRect(Math.random() * 100,10,100,100);
+    //2 (xy) dimension loop
+    for (let x=0; x < iterations; x++){
+      for (let y = 0; y < iterations; y++){
+        const u = x / (iterations - 1);
+        const v = y / (iterations - 1);
 
+        //add them to the array
+        points.push([u,v]);
+      }
+
+    }
+
+    //Retun the array 
+    return points;
   };
+  
+  // Call the function that returns an array and store it in points
+  const points = createGrid();
+
+
+  //declare a margin
+  const margin = 1024 / 10;
+  var totalElements = 0;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'brown';
+    context.fillRect(0,0,width,height);
+
+    points.forEach(([u,v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      //configure and draw the line
+      context.beginPath();
+      context.arc(x + margin, y + margin, 40, Math.PI * 2, false);
+      context.strokeStyle = 'black';
+      context.lineWidth = 10;
+      context.stroke();
+
+      totalElements++;
+
+    });
+    console.log('Total elemetns ' + totalElements.toString())
+
+      
+  };
+
 };
 
 canvasSketch(sketch, settings);
+
+
+//
+/*
+
+
+
+
+*/
