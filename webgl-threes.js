859d63678fc18b0776a5c57acd98edb3b642ed9b
Geoff Hill <geoffwhill@gmail.com>
2019-01-07 19:48:27 +1100

    First Three.js project.

---
+++ webgl-threes.js
@@ -0,0 +1,104 @@
+const canvasSketch = require('canvas-sketch');
+
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require('three');
+
+// Include any additional ThreeJS examples below
+//require('three/examples/js/controls/OrbitControls');
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: 'webgl',
+  // Turn on MSAA
+  attributes: { antialias: true }
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    context
+  });
+
+  // WebGL background color
+  renderer.setClearColor('#FFF', 1);
+
+  // Setup a camera
+  const camera = new THREE.OrthographicCamera();
+  camera.position.set(2, 2, -4);
+  camera.lookAt(new THREE.Vector3()); //look at 0,0,0
+
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+for(let i=0; i < 40; i++){
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({
+      color: 'orange',
+    })
+  );
+  const variance = 1;
+  mesh.position.set(Math.random()*variance,Math.random()*variance,Math.random()*variance );
+  mesh.scale.multiplyScalar(.1);
+  scene.add(mesh);
+}
+
+ 
+
+  /* DELETED LIGHTING
+  // Specify an ambient/unlit colour
+  scene.add(new THREE.AmbientLight('#59314f'));
+
+  // Add some light
+  const light = new THREE.PointLight('#45caf7', 1, 15.5);
+  light.position.set(2, 2, -4).multiplyScalar(1.5);
+  scene.add(light);
+
+  */
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight);
+    
+      // ...
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 1.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render ({ time }) {
+      //mesh.rotation.y = time * (100 * Math.PI / 180);
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload () {
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
